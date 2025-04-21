dd6572959ced6ff1beb04a207caa5f1b5091a2c7
Cosmo <rluke42@hotmail.co.uk>
2025-04-21 12:44:26 +0100

    Docker init commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello</h1>
+        <h2>Luke was here</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
