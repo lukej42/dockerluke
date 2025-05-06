3bf021375f337a6b7ca25b9df45652b5dae4ad0f
lukej42 <61839881+lukej42@users.noreply.github.com>
2025-05-06 11:46:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <h3>This is an Azure App Service website deployed as a Docker container using Azure Devops for CICD</h3>
         <h3>This has been redeployed from my Macbook</h3>
         <h4>GR8</h4>
+        <h4>GR888888</h4>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
